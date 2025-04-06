09bfa3a926198396131d1e5586d358478355e8fc
Giovanni Lira <g.lira99@outlook.com>
2025-04-06 19:49:53 -0300

    Update CI/CD 6

--- main.py
+++ main.py
@@ -4,26 +4,32 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+
 class Estudante(BaseModel):
     name: str
     curso: str
     ativo: bool
 
+
 @app.get("/helloworld")
 async def root():
     return {"Hello": "World"}
+
 
 @app.get("/funcaoteste")
 async def funcaoteste():
     return {"teste": True, "num_aleatorio": random.randint(0, 1000)}
 
+
 @app.post("/estudantes/cadastro")
 async def create_estudante(estudante: Estudante):
     return estudante
 
+
 @app.put("/estudante/update/{id_estudante}")
 async def update_item(id_estudante: int):
     return id_estudante > 0
+
 
 @app.delete("/estudante/delete/{id_estudante}")
 async def delete_estudante(id_estudante: int):
---
+++ test/test_main.py
@@ -0,0 +1,3 @@
+# test_main.py
+def test_exemplo():
+    assert 1 + 1 == 2
