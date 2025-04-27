5788dfcd655839b288bc34daea2a421f183cd39e
Giovanni Lira <g.lira99@outlook.com>
2025-04-27 17:27:09 -0300

    Teste 2

--- test/test.py
+++ test/test.py
@@ -12,7 +12,7 @@ def test_funcaoteste():
 
 def test_create_estudante(estudante: Estudante):
     estudante_teste = Estudante(name="Adriano", curso="Matemática", ativo=False)
-    assert estudante_teste == create_estudante()
+    assert estudante_teste == create_estudante(estudante_teste)
 
 
 def test_update_estudante_negativo():
