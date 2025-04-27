b194c559bfdded662030cca1751a3a84edd95720
Giovanni Lira <g.lira99@outlook.com>
2025-04-27 17:34:29 -0300

    Teste 3

--- test/test.py
+++ test/test.py
@@ -2,27 +2,40 @@ from src.main import *
 from unittest.mock import patch
 
 def test_root():
-    assert root() == {"Hello": "World"}
+    result = root()
+    yield result
+    assert result == {"Hello": "World"}
 
 def test_funcaoteste():
     with patch("dandom.randint", return_value=12345):
         result = funcaoteste()
+        yield result
     assert result == {"teste": True, "num_aleatorio": 12345}
 
 
 def test_create_estudante(estudante: Estudante):
     estudante_teste = Estudante(name="Adriano", curso="Matemática", ativo=False)
-    assert estudante_teste == create_estudante(estudante_teste)
+    result = create_estudante(estudante_teste)
+    yield result
+    assert estudante_teste == result
 
 
 def test_update_estudante_negativo():
-    assert not update_estudante(-5)
+    result = update_estudante(-5)
+    yield result
+    assert not result
 
 def test_update_estudante_positivo():
-    assert not update_estudante(10)
+    result = update_estudante(10)
+    yield result
+    assert not result
 
 def test_delete_estudante_negativo():
-    assert not delete_estudante(-5)
+    result = delete_estudante(-5)
+    yield result
+    assert not result
 
 def test_delete_estudante_positivo():
-    assert not delete_estudante(10)
+    result = delete_estudante(10)
+    yield result
+    assert not result
